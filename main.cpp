57390fa8bd53da1c61b0178854732619223c0b3d
david <davidjerman3@gmail.com>
2022-06-14 10:37:41 +0200

    Added water

--- main.cpp
+++ main.cpp
@@ -27,12 +27,14 @@ class ResourceContainer {
 private:
     std::vector<uint32_t> earthColorPalette = {
             // BGR color format
-            0xff213c51, 0xff28455a, 0xff285a33, 0xff399e28
+            0xff213c51, 0xff28455a, 0xff285a33, 0xff399e28,
+            0xff4390c9, 0xff94cfef
     };
     std::vector<uint32_t> treeColorPalette = {
             // BGR color format - 4. is bark color, 1-3 leaves color
             0xff2aa220, 0xff2aa23a, 0xff2bc311, 0xff143a69
     };
+    uint32_t waterColor = 0xffb0811e;
 public:
     ResourceContainer() = default;
 
@@ -44,6 +46,10 @@ public:
 
     uint32_t getTreeColor(int index) {
         return treeColorPalette[index];
+    }
+
+    uint32_t getWaterColor() {
+        return waterColor;
     }
 };
 
@@ -94,6 +100,12 @@ class World : public olc::PixelGameEngine {
     static const int TREE_FREQ = 120;
     static const int TREE_BARK_HIDE_OFFSET = 15;
 
+    double minLandHeight{};
+    double maxLandHeight{};
+    double avgLandHeight{};
+
+    int waterBoundHeight{};
+
 public:
     World() {
         sAppName = "2D World Generation";
@@ -119,7 +131,7 @@ public:
         if (GetKey(olc::SPACE).bPressed) {
             seed += 1;
             Lehmer32 rnd(seed);
-            noiseArray = getNoiseArray(ScreenWidth(), rnd, 100, ScreenHeight() - 100, 2, 8);
+            noiseArray = getNoiseArray(ScreenWidth(), rnd, 100, ScreenHeight() - 100, 2, 12);
             for (auto &tree: treeList)
                 delete tree;
             treeList = getTreeList(ScreenWidth(), TREE_FREQ, noiseArray, resources, rnd);
@@ -129,7 +141,7 @@ public:
         if (GetKey(olc::C).bHeld) {
             seed += 1;
             Lehmer32 rnd(seed);
-            noiseArray = getNoiseArray(ScreenWidth(), rnd, 100, ScreenHeight() - 100, 2, 8);
+            noiseArray = getNoiseArray(ScreenWidth(), rnd, 100, ScreenHeight() - 100, 2, 12);
             for (auto &tree: treeList)
                 delete tree;
             treeList = getTreeList(ScreenWidth(), TREE_FREQ, noiseArray, resources, rnd);
@@ -150,40 +162,65 @@ public:
         // Draw the noise array - this is the ground
         for (int i = 0; i < noiseArray.size(); i++) {
             auto sub1 = ScreenHeight() - noiseArray[i];
+            bool isWater = (noiseArray[i]) > waterBoundHeight;
             for (int j = 0; j <= ScreenHeight() - noiseArray[i]; j++) {
                 int index;
                 double percentage = (double) j / (double) sub1;
                 if (percentage > 0.4) index = 0;
                 else index = 1;
-                if (j > 12 && j < 45) index = 2;
-                else if (j <= 12) index = 3;
+                if (j > 12 && j < 45) {
+                    if (isWater) index = 4;
+                    else index = 2;
+                }
+                else if (j <= 12) {
+                    if (isWater) index = 5;
+                    else index = 3;
+                }
                 olc::Pixel pixel{resources.getEarthColor(index)};
                 Draw(i, (int) noiseArray[i] + j, pixel);
+            }
+        }
+
+        // Draw the water
+        for (int i = 0; i < ScreenWidth(); i++) {
+            if (noiseArray[i] > waterBoundHeight) {
+                for (int j = waterBoundHeight; j < noiseArray[i]; j++) {
+                    olc::Pixel pixel{resources.getWaterColor()};
+                    Draw(i, j, pixel);
+                }
+            }
+        }
+
+        // Post-processing
+        for (int i = 0; i < ScreenWidth(); i++) {
+            int y = (int)noiseArray[i];
+            if (y > waterBoundHeight - 5 && y < waterBoundHeight + 5) {
+                // TODO: Smooth out the terrain transmission
             }
         }
 
         return true;
     }
 
-    static res::NoiseArray
+    res::NoiseArray
     getNoiseArray(size_t size, Lehmer32 &lehmer, const int startRangeFrom, const int startRangeTo, const int range = 2,
                   const double SMOOTH_FACTOR = 8, const double VEL_RATIO = -1.0) {
 
-        res::NoiseArray noiseArray(size);
-        noiseArray[0] = lehmer.rndInt(startRangeFrom, startRangeTo);
+        res::NoiseArray noiseArr (size);
+        noiseArr[0] = lehmer.rndInt(startRangeFrom, startRangeTo);
 
         double vel = 0;
 
         // Create the noise array
         for (size_t i = 1; i < size; i++) {
-            auto from = noiseArray[i - 1] - range;
-            auto to = noiseArray[i - 1] + range;
-            noiseArray[i] = lehmer.rndDouble(from - vel, to + vel);
+            auto from = noiseArr[i - 1] - range;
+            auto to = noiseArr[i - 1] + range;
+            noiseArr[i] = lehmer.rndDouble(from - vel, to + vel);
 
             // Bounds check
-            if (noiseArray[i] < UPPER_BOUND) noiseArray[i] = lehmer.rndDouble(UPPER_BOUND, UPPER_BOUND + range);
-            else if (noiseArray[i] > LOWER_BOUND)
-                noiseArray[i] = lehmer.rndDouble(LOWER_BOUND - range, LOWER_BOUND);
+            if (noiseArr[i] < UPPER_BOUND) noiseArr[i] = lehmer.rndDouble(UPPER_BOUND, UPPER_BOUND + range);
+            else if (noiseArr[i] > LOWER_BOUND)
+                noiseArr[i] = lehmer.rndDouble(LOWER_BOUND - range, LOWER_BOUND);
 
             // Velocity change
             auto acc = lehmer.rndDouble(-vel * 0.1, vel * 0.1) + vel;
@@ -197,15 +234,29 @@ public:
             double avg = 0;
             int c = 0;
             for (int k = 0; k < SMOOTH_FACTOR && (j - k) > 0; k++, c++)
-                avg += noiseArray[j - k];
+                avg += noiseArr[j - k];
             if (c)
                 avg /= c;
             else
                 continue;
-            noiseArray[j] = avg;
+            noiseArr[j] = avg;
         }
 
-        return noiseArray;
+        avgLandHeight = 0;
+        minLandHeight = ScreenHeight();
+        maxLandHeight = 0;
+
+        for (const auto &noise: noiseArr) {
+            avgLandHeight += noise;
+            if (noise < minLandHeight) minLandHeight = noise;
+            if (noise > maxLandHeight) maxLandHeight = noise;
+        }
+
+        avgLandHeight /= noiseArr.size();
+
+        waterBoundHeight = (int)((2 *avgLandHeight + maxLandHeight) / 3);
+
+        return noiseArr;
     }
 
     // drawTree(new res::Tree(100, 100, 16, 30, 10, resources.getTreeColor(3), resources.getTreeColor(0)));
@@ -216,20 +267,22 @@ public:
     }
 
     res::TreeList
-    getTreeList(int size, int frequency, res::NoiseArray &noiseArray, ResourceContainer &resources, Lehmer32 &rnd) {
+    getTreeList(int size, int frequency, res::NoiseArray &noiseArr, ResourceContainer &resources, Lehmer32 &rnd) {
         res::TreeList tList;
         int x = 40;
         x += rnd.rndInt(frequency / 2, frequency / 2 * 3);
         while (x < ScreenWidth() - 40) {
-            auto y = noiseArray[x];
-            auto w = rnd.rndInt(6, 14);
-            auto h = rnd.rndInt(36, 56) + TREE_BARK_HIDE_OFFSET;
-            auto r = rnd.rndDouble(2.1, 2.6) * w;
-            auto leaf = rnd.rndInt(0, 3);
-            auto bark = 3;
-            tList.push_back(
-                    new res::Tree(x, (int) y, (int) r, h, w, resources.getTreeColor(bark),
-                                  resources.getTreeColor(leaf)));
+            if (noiseArr[x] < avgLandHeight) {
+                auto y = noiseArr[x];
+                auto w = rnd.rndInt(6, 14);
+                auto h = rnd.rndInt(36, 56) + TREE_BARK_HIDE_OFFSET;
+                auto r = rnd.rndDouble(2.1, 2.6) * w;
+                auto leaf = rnd.rndInt(0, 3);
+                auto bark = 3;
+                tList.push_back(
+                        new res::Tree(x, (int) y, (int) r, h, w, resources.getTreeColor(bark),
+                                      resources.getTreeColor(leaf)));
+            }
             x += rnd.rndInt(frequency / 2, frequency / 2 * 3);
         }
         return tList;
@@ -238,7 +291,7 @@ public:
 
 int main() {
 
-    if (World demo; demo.Construct(2048, 1024, 1, 1))
+    if (World demo; demo.Construct(1864, 1024, 1, 1))
         demo.Start();
 
     return 0;
